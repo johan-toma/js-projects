0e2f8045b754ffcf48705557e1feefab1c1a5b90
Johan Toma <98798563+johan-toma@users.noreply.github.com>
2025-04-20 22:53:57 +0200

    add: init javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+//no username + no password 
+//no password but username written 
+//password written but no username
+//password to short (<8)
+//password has no special characters
+//password has no capital letters
+//password has no numbers
+
+const userValidation = document.querySelector('.user-validation')
+const password = document.querySelector('#password').value
+const username = document.querySelector('#username').value
+
