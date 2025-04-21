92cb9b1385a0f72f73e151410933c1c57872ce83
Johan Toma <98798563+johan-toma@users.noreply.github.com>
2025-04-21 05:05:43 +0200

    fix: javascript

--- script.js
+++ script.js
@@ -6,7 +6,33 @@
 //password has no capital letters
 //password has no numbers
 
-const userValidation = document.querySelector('.user-validation')
-const password = document.querySelector('#password').value
-const username = document.querySelector('#username').value
+const userValidation = document.querySelector('.user-validation');
+const password = document.querySelector('#password');
+const username = document.querySelector('#username');
+const form = document.querySelector('.form');
+
+const checks = [
+    /[a-z]/,
+    /[A-Z]/,
+    /\d/,
+    /[@.#$!%^&*.?]/
+];
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    validateInputs();
+})
+
+const validateInputs = () => {
+    const passwordValue = password.value.trim();
+    const usernameValue = username.value.trim();
+
+    if(usernameValue === '') {
+
+    }
+
+    if(passwordValue === '') {
+        
+    }
+}
 
