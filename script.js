b2d366d92b74bbdabb045e8ee4c7084c589cc3b3
Johan Toma <98798563+johan-toma@users.noreply.github.com>
2025-04-21 21:07:55 +0200

    add: finished javascript part

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-//no username + no password 
-//no password but username written 
-//password written but no username
-//password to short (<8)
-//password has no special characters
-//password has no capital letters
-//password has no numbers
+//no username + no password  x
+//no password but username written x 
+//password written but no username x
+//password to short (<8)  x
+//password has no special characters x
+//password has no capital letters x
+//password has no numbers x
 
 const userValidation = document.querySelector('.user-validation');
 const password = document.querySelector('#password');
 const username = document.querySelector('#username');
 const form = document.querySelector('.form');
 
-const checks = [
-    /[a-z]/,
-    /[A-Z]/,
-    /\d/,
-    /[@.#$!%^&*.?]/
-];
+const regex = /^[a-zA-Z0-9!@#$^&*?+-_]{8,}$/;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -27,12 +22,20 @@ const validateInputs = () => {
     const passwordValue = password.value.trim();
     const usernameValue = username.value.trim();
 
-    if(usernameValue === '') {
-
+    if(usernameValue === '' && passwordValue === '') {
+        userValidation.textContent = "Username and password-fields are empty";
     }
-
-    if(passwordValue === '') {
-        
+    else if(usernameValue !== '' && passwordValue === '') {
+        userValidation.textContent = "Password-field cannot be left empty";
     }
+    else if(usernameValue === '' && passwordValue !== '') {
+        userValidation.textContent = "Username-field cannot be left empty";
+    }
+    else {
+        if(regex.test(passwordValue) === false) {
+            userValidation.textContent = "Password incorrect: make sure it fits to the standards of above 8 characters, one special character, one small letter, one capitalized letter, one number...";
+        }
+    }
+    
 }
 
