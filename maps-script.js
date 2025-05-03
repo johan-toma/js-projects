ec655af36aae04e26a6cb09702bdb79d1263cd6a
Johan Toma <98798563+johan-toma@users.noreply.github.com>
2025-05-03 07:15:16 +0200

    add: search functionalities

--- maps-script.js
+++ maps-script.js
@@ -1,36 +1,66 @@
-
 let map;
-
+let marker;
+let infoWindow;
+let center = { lat: 59.33258, lng: 18.0652 };
 async function initMap() {
-  // The location of Stockholm - Segelstorgsfontänen
-  const position = { lat: 59.33258, lng: 18.0652 };
-  const { Map, InfoWindow} = await google.maps.importLibrary("maps");
-  const infoWindow = new InfoWindow();
 
-  map = new Map(document.getElementById("map"), {
-    zoom: 20,
-    center: position,
-    mapId: "Google-Maps-Clone",
-  });
+    const [{ Map }, { AdvancedMarkerElement }] = await Promise.all([
+        google.maps.importLibrary("marker"),
+        google.maps.importLibrary("places")
+    ]);
 
-  const marker = new google.maps.Marker({
-    position: position,
-    map,
-    title: "hello",
-    icon: {
-      url: "mymarker.svg",
-      scaledSize: new google.maps.Size(64, 32)
-    },
-    animation: google.maps.Animation.DROP,
-    draggable: true,
+    map = new google.maps.Map(document.getElementById('map'), {
+        center,
+        zoom: 13,
+        mapId: '4504f8b37365c3d0',
+        mapTypeControl: false,
+        animation: google.maps.Animation.DROP,
+        icon: {
+          url: "mymarker.svg",
+          scaledSize: new google.maps.Size(64, 32)
+        },
+    });
 
-  });
+    const placeAutocomplete = new google.maps.places.PlaceAutocompleteElement();
 
+    placeAutocomplete.id = 'place-autocomplete-input';
+    placeAutocomplete.locationBias = center;
+    const card = document.getElementById('place-autocomplete-card');
 
+    card.appendChild(placeAutocomplete);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
 
+    marker = new google.maps.marker.AdvancedMarkerElement({
+        map,
+    });
+    infoWindow = new google.maps.InfoWindow({});
 
+    placeAutocomplete.addEventListener('gmp-select', async ({ placePrediction }) => {
+        const place = placePrediction.toPlace();
+        await place.fetchFields({ fields: ['displayName', 'formattedAddress', 'location'] });
+        if (place.viewport) {
+            map.fitBounds(place.viewport);
+        }
+        else {
+            map.setCenter(place.location);
+            map.setZoom(17);
+        }
+        let content = '<div id="infowindow-content">' +
+            '<span id="place-displayname" class="title">' + place.displayName + '</span><br />' +
+            '<span id="place-address">' + place.formattedAddress + '</span>' +
+            '</div>';
+        updateInfoWindow(content, place.location);
+        marker.position = place.location;
+    });
 }
-
-
+// Helper function to create an info window.
+function updateInfoWindow(content, center) {
+    infoWindow.setContent(content);
+    infoWindow.setPosition(center);
+    infoWindow.open({
+        map,
+        anchor: marker,
+        shouldFocus: false,
+    });
+}
 initMap();
-
